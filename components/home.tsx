df637f37ad05c05a088d839303bb662cec2e06fb
Hmcruz02 <hmcruz02@gmail.com>
2023-05-30 15:26:08 -0500

    Added picture and files for Home section

--- components/home.tsx
+++ components/home.tsx
@@ -1,23 +1,14 @@
 "use client";
 import React, {useState} from "react";
+import Link from "next/link";
 
 export default function Home() {
     return (
         <section id="home" className="">
             <div className="">
-                <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+                <h1 className="text-4xl font-bold my-2">Howdy!</h1>
+                <h1 className="text-4xl font-bold my-2">I'm <span>Herson </span></h1>
+                <img src="/headshot.jpg" alt="Picture of Herson" className="rounded-2xl border-2 border-gray-900 my-2"/>
             </div>
 
         </section>
